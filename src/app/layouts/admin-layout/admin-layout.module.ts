313b82da43924bb2bcb02117c26fd61e3d6bbc7f
Charuni Kaushalya <snkaushi@gmail.com>
2018-07-01 13:05:16 +0530

    form changes

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -19,6 +19,7 @@ import {
   imports: [
     CommonModule,
     RouterModule.forChild(AdminLayoutRoutes),
+    ReactiveFormsModule,
     FormsModule,
     MatButtonModule,
     MatRippleModule,
--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { MyService } from "../../theme/services/backend/services";
+import { Router, Params, ActivatedRoute } from '@angular/router';
+declare var require: any;
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/map';
+
+import { FormGroup, AbstractControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
+  providers: [MyService],
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
+  ChainInfo = null;
+  public form: FormGroup;
+  public name: AbstractControl;
+  public email: AbstractControl;
 
-  constructor() { }
+
+  constructor(fb: FormBuilder,private _service: MyService,private _route: ActivatedRoute, private _router: Router) { 
+    console.log("user profile");
+    _service.getinfo().then(data => {
+      console.log(data);
+  }).catch(error => {
+      console.log(error.message);
+  });
+
+  
+  _service.getaddresses().then(data => {
+    console.log(data);
+}).catch(error => {
+    console.log("error.message");
+});
+
+this.form = fb.group({
+  'name': ['', Validators.compose([Validators.required, Validators.minLength(1)])],
+  'email': ['', Validators.compose([Validators.required, Validators.minLength(1)])]
+});
+this.name = this.form.controls['name'];
+this.email = this.form.controls['email'];
+  }
 
   ngOnInit() {
+  }
+
+  public onUserSubmit(values: Object): void {
+    console.log("submitted");
+    console.log(values);
   }
 
 }
