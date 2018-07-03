630099b314a4b2739f98b66a6821f04cf9bea5fd
CharuniKaushalya <snkaushi@gmail.com>
2018-07-03 21:02:25 +0530

    user list changes

--- src/app/pages/user-list/user-list.component.ts
+++ src/app/pages/user-list/user-list.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from "../../theme/models/user";
+import { MyService } from "../../theme/services/backend/services";
 
 @Component({
   selector: 'app-table-list',
   templateUrl: './user-list.component.html',
+  providers: [MyService],
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent implements OnInit {
 
-  constructor() { }
+  users: User[] = [];
+  userStream: string = "user";
+
+  constructor(private _service: MyService) { }
 
   ngOnInit() {
+  }
+
+
+  LoadUsers(){
+    this._service.listStreamItems(this.userStream).then(data => {
+      data.forEach(element => {
+        let user: User = JSON.parse(this._service.Hex2String(element.data.toString()));
+        user.user_id = element.txid;
+        this.users.push(user);
+      });
+      console.log(this.users);
+    }).catch(error => {
+      console.log(error.message);
+    });
   }
 
 }
