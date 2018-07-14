a9cfb83d1aac7554ac42d12ecc8c8d4b7ae119bf
CharuniKaushalya <snkaushi@gmail.com>
2018-07-14 23:50:26 +0530

    added contract model

---
+++ src/app/theme/models/contract.ts
@@ -0,0 +1,16 @@
+/**
+ * Contract
+ */
+export class Contract {
+    public contract_id: string;
+    public name: string;
+    public receiver: string;
+    public receiver_email: string;
+    public milestones: number;
+    public milestoneOne: any;
+    public milestoneTwo: any;
+    public milestoneThree: any;
+    public milestoneFour: any;
+    public description: string;
+    public status: any;
+}
